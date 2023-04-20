6d6693006a6bec9b30f4420e0862608d6af4c452
Iryna Litvinova <lytvynlysko@gmail.com>
2023-04-20 12:42:11 +0300

    Change video.js

--- js/video.js
+++ js/video.js
@@ -1,16 +1,11 @@
 (function () {
   const video = document.getElementById("video");
-  const play = document.querySelector(".video-btn");
-
-  function playVid() {
-    video.play();
-    play.style.display = "none";
+  function videoStatus() {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
   }
-
-  function pauseVid() {
-    video.pause();
-    play.style.display = "block";
-  }
-  play.addEventListener('click', playVid);
-  video.addEventListener('click', pauseVid);
+  video.addEventListener('click', videoStatus);
 })();
